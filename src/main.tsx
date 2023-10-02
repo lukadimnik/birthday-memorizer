b8e4b670d6dbdc8f731f3bf18725f4730b290bf2
Luka <luka.dimnik88@gmail.com>
2023-10-02 07:59:16 +0300

    Add auth provider to the app

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,37 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Login from './components/Login.tsx';
 import Whoopsie from './components/Whoopsie.tsx';
+import { AuthProvider, RequireAuth } from 'react-auth-kit';
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <App />,
+    path: '/',
+    element: (
+      <RequireAuth loginPath='/login'>
+        <App />
+      </RequireAuth>
+    ),
     errorElement: <Whoopsie />,
   },
   {
-    path: "/login",
+    path: '/login',
     element: <Login />,
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <AuthProvider
+      authType={'cookie'}
+      authName={'_auth'}
+      cookieDomain={window.location.hostname}
+      cookieSecure={window.location.protocol === 'https:'}
+    >
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </React.StrictMode>
+);
