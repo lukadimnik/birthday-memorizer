b20597b2a7e85527507dc73e7b53d104d7cdbe39
Luka <luka.dimnik88@gmail.com>
2023-10-04 20:52:57 +0300

    Refactor the modal component

--- src/components/UI/Modal.tsx
+++ src/components/UI/Modal.tsx
@@ -1,40 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Modal from 'react-modal';
+import './Modal.css';
 
-const OVERLAY_STYLE: React.CSSProperties = {
-  position: 'fixed',
-  top: 0,
-  left: 0,
-  right: 0,
-  bottom: 0,
-  transform: 'translate(-50%, -50%)',
-  backgroundColor: 'rgba(0,0,0,.7)',
-  zIndex: 1000,
-  border: '1px solid red',
-};
+interface ModalProps {
+  title: string;
+  isOpen: boolean;
+  closeModal: () => void;
+  children: React.ReactNode;
+}
 
-const MODAL_STYLES: React.CSSProperties = {
-  position: 'fixed',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  backgroundColor: '#FFF',
-  padding: '50px',
-  zIndex: 1000,
-  border: '1px solid red',
-};
-
-const Modal = ({ children, closeModal }) => {
-  return ReactDOM.createPortal(
-    <React.Fragment>
-      <div style={OVERLAY_STYLE} />
-      <div style={MODAL_STYLES}>
-        <button onClick={closeModal}>Close</button>
-        {children}
-      </div>
-    </React.Fragment>,
-    document.getElementById('portal') as Element
+const CustomModal: React.FC<ModalProps> = ({ title, isOpen, closeModal, children }) => {
+  return (
+    <Modal isOpen={isOpen} onRequestClose={closeModal}>
+      <h2>{title}</h2>
+      {children}
+      <button onClick={closeModal}>Close Modal</button>
+    </Modal>
   );
 };
 
-export default Modal;
+export default CustomModal;
