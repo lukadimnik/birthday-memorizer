2503aab8623bc948623af79d743a9fccaa0f6ecd
Luka <luka.dimnik88@gmail.com>
2024-02-12 20:01:23 +0200

    Add token token to get requests

--- src/api/birthdayService.ts
+++ src/api/birthdayService.ts
@@ -1,32 +1,34 @@
 import axios from 'axios';
 import { NewBirthday } from '../models/interfaces';
 import Cookies from 'js-cookie';
+
 const baseUrl = '/api/birthdays';
 
+const api = axios.create({
+  baseURL: baseUrl,
+  headers: {
+    Authorization: `Bearer ${Cookies.get('_auth')}`,
+  },
+});
+
 export const getBirthday = (id: number) => {
-  return axios.get(`${baseUrl}/${id}`);
+  return api.get(`/${id}`);
 };
 
 export const getAllBirthdays = () => {
-  return axios.get(baseUrl);
+  return api.get('/');
 };
 
 export const createBirthday = (newBirthday: NewBirthday) => {
-  return axios.post(baseUrl, newBirthday, {
-    headers: { Authorization: `Bearer ${Cookies.get('_auth')}` },
-  });
+  return api.post('/', newBirthday);
 };
 
 export const updateBirthday = (id: number, newBirthday: NewBirthday) => {
-  return axios.put(`${baseUrl}/${id}`, newBirthday, {
-    headers: { Authorization: `Bearer ${Cookies.get('_auth')}` },
-  });
+  return api.put(`/${id}`, newBirthday);
 };
 
 export const deleteBirthday = (id: number) => {
-  return axios.delete(`${baseUrl}/${id}`, {
-    headers: { Authorization: `Bearer ${Cookies.get('_auth')}` },
-  });
+  return api.delete(`/${id}`);
 };
 
 export const login = (username: string, password: string) => {
